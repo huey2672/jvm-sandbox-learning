24e7f7b79b0115cac92dfedba3a3f7d84f522dc2
huey2672 <huey2672@163.com>
2022-06-08 10:09:22 +0800

    commit

--- jvm-sandbox-quickstart/clock-tinker/src/main/java/com/huey/learning/jvmsandbox/quickstart/BrokenClockTinkerModule.java
+++ jvm-sandbox-quickstart/clock-tinker/src/main/java/com/huey/learning/jvmsandbox/quickstart/BrokenClockTinkerModule.java
@@ -21,25 +21,21 @@ public class BrokenClockTinkerModule implements Module {
 
     @Command("repairCheckState")
     public void repairCheckState() {
-
         new EventWatchBuilder(moduleEventWatcher)
                 .onClass("com.huey.learning.jvmsandbox.quickstart.Clock")
                 .onBehavior("checkState")
                 .onWatch(new AdviceListener() {
-
                     /**
-                     * 拦截{@code com.taobao.demo.Clock#checkState()}方法，当这个方法抛出异常时将会被
-                     * AdviceListener#afterThrowing()所拦截
+                     * 拦截 {@code com.huey.learning.jvmsandbox.quickstart.Clock#checkState()} 方法，
+                     * 当这个方法抛出异常时会被 AdviceListener#afterThrowing() 所拦截
                      */
                     @Override
                     protected void afterThrowing(Advice advice) throws Throwable {
-
-                        // 在此，你可以通过ProcessController来改变原有方法的执行流程
-                        // 这里的代码意义是：改变原方法抛出异常的行为，变更为立即返回；void返回值用null表示
+                        // 在此，你可以通过 ProcessController 来改变原有方法的执行流程
+                        // 这里的代码意义是：改变原方法抛出异常的行为，变更为立即返回；void 返回值用 null 表示
                         ProcessController.returnImmediately(null);
                     }
                 });
-
     }
 
 }
